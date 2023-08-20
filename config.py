8a27970383254cb6e22da747d4671132807087f5
fatom3 <136278463+fatom3@users.noreply.github.com>
2023-08-20 04:11:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    API_ID = int(os.getenv("API_ID", ))
-    API_HASH = os.getenv("API_HASH", '')
-    BOT_TOKEN = os.getenv("BOT_TOKEN", '')
+    API_ID = 20993785
+    API_HASH = "a5378e174b86b9fc3cf1ef284e2767b4"
+    BOT_TOKEN = "6277898727:AAFDM0H6__vq8xviJ662iMcaOqUBIGPqLNo"
